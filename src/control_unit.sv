3eaed74b6d5406fe13114284487a5d664cecec8f
Leonardo-Mittmann <leonardo-pagnossin@uergs.edu.br>
2022-11-24 23:22:37 -0300

    Datapath e Início da Unidade de Controle

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -21,20 +21,64 @@ import k_and_s_pkg::*;
     output logic                    halt
 );
 
-// remove from here
-// signal added to test environment..
-logic [7:0] counter = 'd0;
+typedef enum{
+    FETCH_INSTRUCTION,
+    REGISTER_INSTRUCTION,
+    DECODE,
+    S_LOAD_1,
+    S_LOAD_2,
+    S_HALT
+}state_t;
 
-//process to test environment ... remove this
-always @(posedge clk or negedge rst_n) begin
-    if (~rst_n)
-        counter <= 'd0;
-    else
-        counter <= counter + 1;
+state_t state;
+state_t next_state;
+
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        state <= FETCH_INSTRUCTION;
+    end
+    else begin
+         state <= next_state;
+    end
 end
-
-assign halt = ( (&(counter)) ? 1'b1 : 1'b0);
-//until here !!!!
-
+    
+    always_comb begin : calc_next_state
+        branch = 1'b0;
+        pc_enable = 1'b0;
+        ir_enable = 1'b0;
+        write_reg_enable = 1'b0;
+        addr_sel = 1'b0;
+        c_sel = 1'b0;
+        operation = 2'b00;
+        flags_reg_enable = 1'b0;
+        ram_write_enable = 1'b0;
+        halt = 1'b0;
+        case (state)
+            FETCH_INSTRUCTION : begin
+                next_state = DECODE;
+                ir_enable = 1'b1;
+                pc_enable = 1'b1;
+            end
+            DECODE: begin
+                next_state = FETCH_INSTRUCTION;
+                case(decoded_instruction)
+                    I_HALT: begin
+                        next_state = S_HALT;
+                    end
+                    I_LOAD: begin
+                        next_state = S_LOAD_1;
+                        addr_sel = 1'b1;
+                    end
+            end
+                endcase
+                
+             S_LOAD_1: begin
+                next_state = S_LOAD_2;
+                addr_sel = 1'b1;
+                c_sel = 1'b1;
+            end 
+        endcase
+    end : calc_next_state //always comb
 
 endmodule : control_unit
--- src/data_path.sv
+++ src/data_path.sv
@@ -21,10 +21,181 @@ import k_and_s_pkg::*;
     input  logic             [15:0] data_in
 
 );
+logic [15:0] bus_a;
+logic [15:0] bus_b;
+logic [15:0] bus_c;
+logic [15:0] instruction;
+logic [15:0] alu_out;
 
-// added to remove warning from testbench
-// remove it
-assign ram_addr = 'd0;
-// remove 
+logic [4:0]  mem_addr;
+logic [4:0]  program_counter;
+
+logic [1:0]  a_addr;
+logic [1:0]  b_addr;
+logic [1:0]  c_addr;
+
+logic zero_f;
+logic neg_f;
+logic overflow_f;
+logic signed_overflow_f;
+logic alu_carry_in;
+
+always_ff @(posedge clk) begin
+    if (ir_enable) begin
+        instruction <= data_in;
+    end
+end
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        program_counter <= 'd0;
+    end
+    else if (pc_enable) begin
+        if (branch) begin
+            program_counter <= mem_addr;
+         end else begin
+            program_counter <= program_counter + 1;
+         end
+    end
+end
+
+always_comb begin
+    case (operation)
+        2'b00: begin //ADD
+            {alu_carry_in, alu_out[14:0]} = bus_a[14:0] + bus_b[14:0];
+            {overflow_f, alu_out[15]} = bus_a[15] + bus_b[15] + alu_carry_in;
+            signed_overflow_f = overflow_f ^ alu_carry_in;
+        end
+        2'b01: begin //AND
+            alu_out = bus_a & bus_b;
+            overflow_f = 1'b0;
+            signed_overflow_f = 1'b0;
+        end
+        2'b10: begin //OR
+            alu_out = bus_a | bus_b;
+            overflow_f = 1'b0;
+            signed_overflow_f = 1'b0;
+        end
+        2'b11: begin //SUB
+            {alu_carry_in, alu_out[14:0]} = bus_a[14:0] + (-bus_b[14:0]);
+            {overflow_f, alu_out[15]} = bus_a[15] + bus_b[15] + alu_carry_in;
+            signed_overflow_f = overflow_f ^ alu_carry_in;
+        end
+        default: begin
+            $display("ERRO: Operacao da ULA invalida");
+            alu_out = 'd1234;
+            overflow_f = 1'b0;
+            signed_overflow_f = 1'b0;
+        end
+     endcase
+end
+
+assign zero_f = ~|(alu_out);
+assign neg_f = alu_out[15];
+assign bus_c = (c_sel?data_in:alu_out);
+assign ram_addr = (addr_sel?mem_addr:program_counter);
+
+always_comb begin
+    a_addr = 'd0;
+    b_addr = 'd0;
+    c_addr = 'd0;
+    mem_addr = 'd0;
+    case (instruction [15:8])
+    //--Movimentação de dados--
+    //LOAD
+    8'b1000_0001: begin
+        c_addr = instruction[6:5];
+        mem_addr = instruction[4:0];
+        decoded_instruction = I_LOAD; 
+    end
+    //STORE
+    8'b1000_0010: begin
+        a_addr = instruction[6:5];
+        mem_addr = instruction[4:0];
+        decoded_instruction = I_STORE;
+    end
+    //MOVE
+    8'b1001_0001: begin
+        a_addr = instruction[1:0];
+        b_addr = instruction[1:0];
+        c_addr = instruction[3:2];
+        mem_addr = instruction[4:0];
+        decoded_instruction = I_MOVE; 
+    end
+    
+    //--Aritimética--
+    //ADD
+    8'b1010_0001: begin
+        decoded_instruction = I_ADD;
+        a_addr = instruction[1:0];
+        b_addr = instruction[3:2];
+        c_addr = instruction[5:4];
+    end
+    //SUB
+    8'b1010_0010: begin
+    decoded_instruction = I_SUB;
+        a_addr = instruction[1:0];
+        b_addr = instruction[3:2];
+        c_addr = instruction[5:4];
+    end
+    
+    //--Lógica--
+    //AND
+    8'b1010_0011: begin
+        decoded_instruction = I_AND;
+        a_addr = instruction[1:0];
+        b_addr = instruction[3:2];
+        c_addr = instruction[5:4];
+    end
+    //OR
+    8'b1010_0100: begin
+        decoded_instruction = I_OR;
+        a_addr = instruction[1:0];
+        b_addr = instruction[3:2];
+        c_addr = instruction[5:4];
+    end
+    //--Desvio
+    //BRANCH
+    8'b0000_0001: begin
+        decoded_instruction = I_BRANCH;
+        mem_addr = instruction[4:0];
+    end
+    //BZERO
+    8'b0000_0010: begin 
+        decoded_instruction = I_BZERO;
+        mem_addr = instruction[4:0];
+    end
+    //BNEG
+    8'b0000_0011: begin
+        decoded_instruction = I_BNEG;
+        mem_addr = instruction[4:0];
+    end
+    //BOV
+    8'b0000_0101: begin
+        decoded_instruction = I_BOV;
+        mem_addr = instruction[4:0];
+    end
+    //BNNEG
+    8'b0000_1010: begin
+        decoded_instruction = I_BNNEG;
+        mem_addr = instruction[4:0];
+    end
+    //BNZERO
+    8'b0000_1011: begin
+        decoded_instruction = I_BNZERO;
+        mem_addr = instruction[4:0];
+    end
+    
+    //Fim de programa
+    //HALT
+    8'b1111_1111: begin
+        decoded_instruction = I_HALT;
+    end
+    //NOP
+    default : begin
+        decoded_instruction = I_NOP;
+    end
+  endcase
+end
 
 endmodule : data_path
